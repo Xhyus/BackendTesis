fe95c42f4b0b316d8fe2ec38ed55eac8ba8db4e8
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-02 20:34:39 -0300

    Generacion modelo servicios

---
+++ models/service.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const serviceSchema = new Schema({
+    service: {
+        type: String,
+        required: true,
+        maxLenght: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLenght: 100
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    itemList: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Item',
+        required: true,
+        maxLenght: 8
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    updated: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
