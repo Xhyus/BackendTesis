e8c08246d10293f7d1b46a14bc3f2860f65d6f0b
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-02 00:42:50 -0300

    Inicialización proyecto

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoutes');
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.options('*', cors())
+app.use('/api', userRoutes);
+
+const options = {
+    useNewUrlParser: true,
+    autoIndex: true,
+    keepAlive: true,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(process.env.DB, options, function (error) {
+    if (error) {
+        return console.log(error)
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+module.exports = app;
