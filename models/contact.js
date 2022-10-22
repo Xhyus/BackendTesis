5704504857bf31dace3df111939fd7517c334fdb
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-22 20:33:55 -0300

    generacion de modelo de contacto

---
+++ models/contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    phone: {
+        type: String,
+        required: true,
+        maxlength: 12
+    },
+    role: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    rut: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 12
+    }
+})
+
+module.exports = mongoose.model('contact', contactSchema);
