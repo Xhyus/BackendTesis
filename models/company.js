1db29866dbade7a87204b8b07581a9854492ebea
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-22 20:34:07 -0300

    Generacion de modelo de compañia

--- models/company.js
+++ models/company.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const companySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    address: {
+        type: String,
+        required: true,
+        maxlength: 250
+    },
+    rut: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 12
+    },
+    phone: {
+        type: String,
+        required: true,
+        maxlength: 12
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    contact: {
+        type: Schema.objectId,
+        ref: 'contact',
+        required: true
+    }
 
 })
 
