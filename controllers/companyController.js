d8a0696368cc7534d4a76ff3d5242c4b794410cf
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-22 21:09:29 -0300

    Generacion controlador para editar y eliminar una empresa

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -40,11 +40,37 @@ const getSpecificCompany = async (req, res) => {
     })
 }
 
+const updateCompany = async (req, res) => {
+    const { id } = req.params;
+    const { name, rut, address, phone, email, contact } = req.body;
+    Company.findByIdAndUpdate(id, { name, rut, address, phone, email, contact }, { new: true }, (err, company) => {
+        if (err) {
+            return res.status(400).send({ message: 'Error al actualizar la empresa' });
+        }
+        if (!company) {
+            return res.status(404).send({ message: 'Empresa no encontrada' });
+        }
+        return res.status(200).send(company);
+    })
+}
+
+const deleteCompany = async (req, res) => {
+    const { id } = req.params;
+    Company.findByIdAndDelete(id, (err, company) => {
+        if (err) {
+            return res.status(400).send({ message: 'Error al eliminar la empresa' });
+        }
+        if (!company) {
+            return res.status(404).send({ message: 'Empresa no encontrada' });
+        }
+        return res.status(200).send(company);
+    })
+}
 
 module.exports = {
     createCompany,
     getCompanies,
     getSpecificCompany,
-    // updateCompany,
-    // deleteCompany
+    updateCompany,
+    deleteCompany
 }
