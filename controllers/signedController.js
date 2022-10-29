2f7e6fdccdae8cdd54ca5e45dc3da8f037749fd6
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-29 19:52:56 -0300

    Controlador para generar paginas firmadas

---
+++ controllers/signedController.js
@@ -0,0 +1,22 @@
+const Signed = require('../models/signed');
+
+const createSigned = (req, res) => {
+    let { type, use, left } = req.body;
+    if (use === 'recovery') {
+        left = 3;
+    } else {
+        left = 2;
+    }
+    const newSigned = new Signed({
+        type,
+        use,
+        left
+    })
+    newSigned.save((err, signed) => {
+        if (err) {
+            return res.status(400).send({ message: "Error al crear la página protegida" });
+        }
+        return res.status(200).send(signed);
+    })
+}
+
