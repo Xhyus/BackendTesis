789ab3430b6c64067eacac95ae90642374dc1667
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-11-11 22:41:09 -0300

    Small fix

--- middleware/checkRUT.js
+++ middleware/checkRUT.js
@@ -2,14 +2,13 @@ const rutlib = require('rutlib')
 const Company = require('../models/company')
 
 const checkRUT = (req, res, next) => {
-    const rut = req.body.rut
-    if (rutlib.validateRut(rut)) {
+    if (rutlib.validateRut(req.body.rut) && rutlib.validateRut(req.body.contactRut)) {
         Company.findOne({ rut }, (err, company) => {
             if (err) {
                 return res.status(500).send({ message: 'Error en la petición' })
             }
             if (company) {
-                return res.status(200).send({ message: 'El RUT ya existe' })
+                return res.status(400).send({ message: 'El RUT ya existe' })
             }
             next()
         })
