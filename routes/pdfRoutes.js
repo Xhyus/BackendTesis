a02f334213e342063ab3079e916f5cf1966aa35e
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-11-13 16:07:46 -0300

    Generacion de pdfs en nodejs

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 const hbs = require('handlebars');
+const PDFMerger = require('pdf-merger-js');
 
 const compile = async (templateName, data) => {
     const filepath = path.join(process.cwd(), 'templates', `${templateName}.hbs`);
@@ -18,24 +19,28 @@ router.get("/pdf", async (req, res) => {
     }
     try {
         const browser = await puppeteer.launch();
+        const merger = new PDFMerger();
         const page = await browser.newPage();
-        const content = await compile('invoice', name);
+        const content = await compile('invoice', name)
         await page.setContent(content);
         await page.emulateMediaType('screen');
         await page.pdf({
             path: 'cotizacion.pdf',
-            format: 'A4',
-            printBackground: true
+            format: 'A3',
+            printBackground: true,
+            landscape: true
         });
+        await merger.add('cotizacion.pdf');
+        await merger.add('cotizacion.pdf');
+        await merger.save('cotizacion.pdf');
         await browser.close();
         return res.sendFile(path.join(__dirname, '../cotizacion.pdf'));
-
     } catch (e) {
         console.log(e)
+        if (fs.existsSync(path.join(__dirname, '../cotizacion.pdf'))) {
+            fs.unlinkSync(path.join(__dirname, '../cotizacion.pdf'));
+        }
     }
-
-
-
 })
 
 module.exports = router;
