d57980a16d77583bf802e41f590cc2a80a1948ea
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-08 00:53:43 -0300

    Generacion de servicio para enviar correos para recuperar contraseñas

--- services/transporter.js
+++ services/transporter.js
@@ -33,6 +33,41 @@ const sendMail = (name) => {
         return console.log('Correo enviado');
     })
 }
+
+const sendMailRecoverPassword = (email, password) => {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    transporter.verify().then(() => {
+        console.log('Servidor de correos listo');
+    }).catch(err => {
+        console.log("Error utilizar servidor de correos");
+    })
+
+    transporter.sendMail({
+        from: `"Sistema de Gestión de Empresas" <${process.env.EMAIL}>`,
+        to: [
+            email
+        ],
+        subject: 'Se ha realizado una recuperación de contraseña',
+        text: `Se ha solicitado una recuperacion de contraseña a las ${new Date().toLocaleString()}, la nueva contraseña es: ${password}`,
+    }, (err, info) => {
+        if (err) {
+            return res.status(400).send({ message: 'Error al enviar el correo' });
+        }
+        return console.log('Correo enviado');
+    })
+
+}
+
 module.exports = {
-    sendMail
+    sendMail,
+    sendMailRecoverPassword
 };
