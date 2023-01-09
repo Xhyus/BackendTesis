f46b8e27623e04b3eeb0eec0048ec6ae1ca4e02d
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-09 20:10:42 -0300

    Se agrega ruta para endpoint

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -7,8 +7,8 @@ api.post('/quote', auth.auth, quotesController.createQuote);
 api.get('/quotes', auth.auth, quotesController.getQuotes);
 api.get('/quotes/active', auth.auth, quotesController.getActiveQuotes);
 api.get('/quote/search/:id', auth.auth, quotesController.getQuote);
-api.put('/quote/update/:id', auth.auth, quotesController.updateQuote);
 api.put('/quote/delete/:id', auth.auth, quotesController.deleteQuote);
 api.get('/quotes/search/company/:id', auth.auth, quotesController.getQuotesByCompany);
+api.get('/quote/search/client/:url', quotesController.getClientQuote);
 
 module.exports = api;
