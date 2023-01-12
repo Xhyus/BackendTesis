331bbf78ed5658e52fde03be0c45f4f678fdecd8
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-11 23:25:02 -0300

    Se corrije error en el endpoint

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -7,7 +7,7 @@ api.post('/quote', auth.auth, quotesController.createQuote);
 api.get('/quotes', auth.auth, quotesController.getQuotes);
 api.get('/quotes/active', auth.auth, quotesController.getActiveQuotes);
 api.get('/quote/search/:id', auth.auth, quotesController.getQuote);
-api.put('/quote/delete/:id', auth.auth, quotesController.deleteQuote);
+api.delete('/quote/delete/:id', auth.auth, quotesController.deleteQuote);
 api.get('/quotes/search/company/:id', auth.auth, quotesController.getQuotesByCompany);
 api.get('/quote/search/client/:url', quotesController.getClientQuote);
 
