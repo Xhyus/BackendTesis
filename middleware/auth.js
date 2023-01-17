d512add56cf235ca414f18e983679d3465b3e624
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-17 16:48:54 -0300

    FIX

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,9 +9,14 @@ const auth = (req, res, next) => {
     }
     jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
         if (err) {
+            console.log(err)
             return res.status(401).send({ message: "Token no válido" })
         }
-        req.user = decoded;
+        console.log(decoded)
+        // console log decoded.iat and decoded.exp with format: new Date(decoded.iat * 1000) and new Date(decoded.exp * 1000)
+        console.log(new Date(decoded.iat * 1000))
+        console.log(new Date(decoded.exp * 1000))
+        req.user = decoded.id;
         next();
     });
 }
