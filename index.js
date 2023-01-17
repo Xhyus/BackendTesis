747116522ae46b4c727590735a79d1dd57f8a5d1
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2023-01-16 21:54:44 -0300

    Se modifica variable de origen para peticiones

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const quoteRoutes = require('./routes/quoteRoutes');
 
 const app = express()
 app.use(cookieParser())
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND }));
 app.use(express.json())
 app.options('*', cors())
 app.use('/api', userRoutes);
